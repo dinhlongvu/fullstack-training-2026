08c1743b02aa75751fac09ca7cfd03ee22441953
wanghoc <trieuquanghocts@gmail.com>
2026-06-23 10:10:03 +0700

    chore(db): add missing migration for project configurations constraints

---
+++ backend/Migrations/20260623030619_AddProjectConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectMembers_ProjectId",
+                table: "ProjectMembers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_CreatedAt",
+                table: "Projects",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_ProjectId_UserId",
+                table: "ProjectMembers",
+                columns: new[] { "ProjectId", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_CreatedAt",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectMembers_ProjectId_UserId",
+                table: "ProjectMembers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_ProjectId",
+                table: "ProjectMembers",
+                column: "ProjectId");
+        }
+    }
+}
